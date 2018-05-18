18be2311e799149916dbb85c4dd1bdaa5a52104a
stormZDG <zhu527625750@163.com>
2018-05-18 14:25:38 +0800

    在MainActitivy 中初始化btn01

--- app/src/main/java/com/example/administrator/demotest/MainActivity.java
+++ app/src/main/java/com/example/administrator/demotest/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.administrator.demotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private String TAG = "TAG";
+    private Button btn01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG, "onCreate: ");
+        init();
 
         
     }
 
+    private void init() {
+        btn01 = findViewById(R.id.btn01);
 
 
+    }
 
 
+    @Override
+    public void onClick(View v) {
 
+    }
+
+    
 }
