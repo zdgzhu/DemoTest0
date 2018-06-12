c82dc2ce42b598c01cbdc083dee1330c1d188eb7
stormZDG <zhu527625750@163.com>
2018-06-12 21:53:42 +0800

    下载更新的功能已经写完

--- app/src/main/java/com/example/service/update/UpdateDownloadListener.java
+++ app/src/main/java/com/example/service/update/UpdateDownloadListener.java
@@ -5,4 +5,40 @@ package com.example.service.update;
  */
 
 public interface UpdateDownloadListener {
+
+    /**
+     * 下载请求开始回调
+     */
+    public void onStarted();
+
+    /**
+     * 请求成功，下载前的准备回调
+     */
+    public void onPrepared(long contentLength, String downloadUrl);
+    /**
+     * 进度更新回调
+     *
+     * @param progress
+     * @param downloadUrl
+     */
+    public void onProgressChanged(int progress, String downloadUrl);
+
+    /**
+     * 下载过程中暂停的回调
+     */
+    public void onPaused(int progress,int completeSize,String downloadUrl);
+
+    /**
+     * 下载完成回调
+     */
+
+    public void onFinished(int completeSize, String downloadUrl);
+
+    /**
+     * 下载失败回调
+     */
+    public void onFailure();
+
+
+
 }
